a2ebfbec8d98aabf06abb67c3e04ddba027192b8
Ian <sebastianvaz123@hotmail.com>
2021-10-26 00:14:34 -0500

    Se hizo el reto de platzi con mi nombre
    Signed-off-by: Ian <sebastianvaz123@hotmail.com>

---
+++ closure.py
@@ -0,0 +1,41 @@
+# Hola 3 -> HolaHolaHola
+# Sebas 2 -> SebasSebas
+# Platzi 4 -> PlatziPlatziPlatziPlatzi
+
+def make_repeater_off(n):
+    def repeater(string):
+        assert type(string) == str, "Solo puedes utilizar cadenas"
+        return string*n
+    return repeater
+
+
+def make_division_by(n):
+    """
+        This closure returns a function that returns the division of an x number by 3
+        """
+    def division(x):
+        assert type(n) == int
+        assert type(x) == int
+        return x/n
+    return division
+
+
+def run():
+    repeat_5 = make_repeater_off(5)
+    print(repeat_5("Hola"))
+    repeat_10 = make_repeater_off(10)
+    print(repeat_10("Sebas"))
+
+    division_by_3 = make_division_by(3)
+    assert division_by_3(15) == 5
+    print(division_by_3(15))
+    division_by_10 = make_division_by(10)
+    assert division_by_10(100) == 10
+    print(division_by_10(100))
+    division_by_4 = make_division_by(4)
+    assert division_by_4(100) == 25
+    print(division_by_4(100))
+
+
+if __name__ == "__main__":
+    run()
