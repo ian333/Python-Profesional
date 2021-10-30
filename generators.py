49b089cdb650218bd1e55f547a3299f0594ba7b1
Ian <sebastianvaz123@hotmail.com>
2021-10-29 22:59:06 -0500

    Se añadio un limite que es puesto por el usuario
    Signed-off-by: Ian <sebastianvaz123@hotmail.com>

--- generators.py
+++ generators.py
@@ -1,6 +1,6 @@
 import time
 
-def fibo_gen():
+def fibo_gen(limit:int) -> int:
     n1=0
     n2=1
     counter=0
@@ -14,11 +14,15 @@ def fibo_gen():
         else:
             aux=n1+n2
             n1,n2=n2,aux
+            if aux > limit: break
             yield aux
 if __name__ == "__main__":
-    fibonacci=fibo_gen()
+    limit=int(input("Ingrese el numero maximo de la susecion: "))
+    fibonacci=fibo_gen(limit)
+    
+
     for element in fibonacci:
         print(element)
-        time.sleep(1)
+        time.sleep(0.1)
 
                         
